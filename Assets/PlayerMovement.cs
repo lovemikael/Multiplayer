a7674c6a513a16429016d2626ca43c94d91e6fa8
LoveBeling <love.beling@vobling.com>
2020-11-16 17:21:49 +0100

    First steps

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if(isLocalPlayer)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHorizontal * 0.1f,moveVertical * 0.1f,0);
+            transform.position += movement; 
+        }
+    }
+}
